ed6f796a6a4cf8739e132b3cd0ba9ba38c546b2e
Victor Defontnouvelle <vdefont@gmail.com>
2018-08-14 14:59:25 -0400

    Axios change

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const API = axios.create({
-  baseURL: 'http://18.222.253.201:8081'
+  baseURL: 'http://172.31.32.52:8081'
 })
 
 export default (query, data = {}) => {
